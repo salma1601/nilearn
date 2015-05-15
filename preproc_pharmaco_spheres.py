24e83a57a8ea397f634dc6291e30b2b88252a34e
Salma BOUGACHA <sb238920@is223297.intra.cea.fr>
2015-05-15 13:12:24 +0200

    typo

--- preproc_pharmaco_spheres.py
+++ preproc_pharmaco_spheres.py
@@ -320,7 +320,8 @@ for n, folder in enumerate(folders):
 
     # PCA components from WM and CSF
     for tissue_mask in binary_masks:
-        tissue_masker = nilearn.input_data.NiftiMasker(tissue_mask)
+        tissue_masker = nilearn.input_data.NiftiMasker(mask_img=tissue_mask)
+        print os.path.isfile(tissue_mask)
         tissue_func = tissue_masker.fit_transform(func_filename)
         tissue_hv_confounds = mem.cache(nilearn.image.high_variance_confounds)(
             tissue_func)

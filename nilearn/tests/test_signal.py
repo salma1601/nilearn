9a847891f056209254eda14d6ac32148a4052805
Salma <salmabougacha@hotmail.com>
2016-01-28 21:17:21 +0100

    added test for constant confound

--- nilearn/tests/test_signal.py
+++ nilearn/tests/test_signal.py
@@ -372,14 +372,15 @@ def test_clean_confounds():
     assert_raises(TypeError, nisignal.clean, signals,
                   confounds=[None])
 
-    # Test without standardizing that constant parts of confounds are
+    # Test with standardizing that constant parts of confounds are
     # accounted for
-    np.testing.assert_almost_equal(nisignal.clean(np.ones((20, 2)),
-                                                  standardize=False,
-                                                  confounds=np.ones(20),
-                                                  detrend=False,
-                                                  ).mean(),
-                                   np.zeros((20, 2)))
+    for normalize in [None, 'psc']:
+        np.testing.assert_almost_equal(nisignal.clean(np.ones((20, 2)),
+                                                      normalize=normalize,
+                                                      confounds=np.ones(20),
+                                                      detrend=False,
+                                                      ).mean(),
+                                       np.zeros((20, 2)))
 
 
 def test_high_variance_confounds():
